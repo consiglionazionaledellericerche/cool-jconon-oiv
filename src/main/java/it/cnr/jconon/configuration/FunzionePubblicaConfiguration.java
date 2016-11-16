56aed2d26a5c7a28f2dfcb4feb9bad292e3dbd73
Francesco Uliana <francesco.uliana@cnr.it>
2016-11-16 19:03:36 +0100

    fix #1 i18n - Evitare il recupero del locale del browser e forzare sempre it

--- src/main/java/it/cnr/jconon/configuration/FunzionePubblicaConfiguration.java
+++ src/main/java/it/cnr/jconon/configuration/FunzionePubblicaConfiguration.java
@@ -1,13 +1,16 @@
 package it.cnr.jconon.configuration;
 
 import it.cnr.cool.service.I18nServiceLocation;
+import it.cnr.cool.service.PageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by francesco on 14/11/16.
@@ -19,6 +22,18 @@ public class FunzionePubblicaConfiguration {
     private static final Logger LOGGER = LoggerFactory.getLogger(FunzionePubblicaConfiguration.class);
 
     public static final String I18N_COOL_JCONON_FP = "i18n.cool-jconon-fp";
+
+    private PageService pageService;
+
+    public FunzionePubblicaConfiguration (PageService pageService) {
+        this.pageService = pageService;
+    }
+
+    @PostConstruct
+    public void overrideLang() {
+        pageService.setOverrideLang(Locale.ITALIAN.getLanguage());
+    }
+
 
     @Bean(name = "jconon-fpI18nServiceLocation")
     public I18nServiceLocation fpI18nServiceLocation() {
