7e79818a1e41072fd01f77bef2f59b012a89b474
marco.spasiano <marco.spasiano@cnr.it>
2017-02-14 17:56:27 +0100

    Aggiunte label

--- src/main/resources/META-INF/js/ws/call/selezione-comparativa.js
+++ src/main/resources/META-INF/js/ws/call/selezione-comparativa.js
@@ -129,7 +129,7 @@ define(['jquery', 'i18n', 'header', 'cnr/cnr.actionbutton', 'cnr/cnr.search',
         customButtons.attachments = function () {
           Call.displayAttachments(el.id);
         };
-        if (isActive) {
+        if (isActive || el.data_inizio_invio_domande === '') {
           customButtons.edit = function () {
             window.location = jconon.URL.call.manage + '?call-type=' + el.objectTypeId + '&cmis:objectId=' + el.id;
           };
